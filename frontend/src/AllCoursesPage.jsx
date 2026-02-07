13530b82cd11ddaca36c5b3c740db24bdd7ae292
shauryasecondry-dev <shauryasecondary@gmail.com>
2026-02-07 17:45:18 +0530

    message

--- frontend/src/AllCoursesPage.jsx
+++ frontend/src/AllCoursesPage.jsx
@@ -1,11 +1,21 @@
 import AllCourses from "./AllCourses.jsx"
 import Navbar from "./Navbar.jsx"
- 
+import SearchFunctionality from "./SearchFunctionality.jsx"
+import {useAuth} from "./context/AuthProvider.jsx"
 function AllCoursesPage(){
+    let {user}=useAuth();
 return (
 <>
+
 <Navbar/>
-<h3>login to purchase</h3>
+{
+    (!user)?<h3 style={{marginBottom:"5vh"}}>login to purchase</h3>:""
+}
+
+{
+    (user)?<SearchFunctionality/>:""
+}
+
 <div style={{marginTop:"17vh"}}>
 <AllCourses ></AllCourses>
 </div>
