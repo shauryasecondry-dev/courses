74e8b39d0a247c0170b73eb9c2d4b4d680e36464
shauryasecondry-dev <shauryasecondary@gmail.com>
2026-02-07 14:25:40 +0530

    message

--- backend/index.js
+++ backend/index.js
@@ -20,8 +20,8 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
  
- app.use(cors({
-  origin: "http://localhost:5173",
+app.use(cors({
+  origin: process.env.FRONTEND_URL||"http://localhost:5173",  // ✅ Use environment variable
   credentials: true
 }));
 
